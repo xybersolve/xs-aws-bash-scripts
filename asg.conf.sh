f17ab3e2da234fddc7f05d3fec13a25dd643fee6
xybersolve <xybersolve@gmail.com>
2018-08-27 15:16:10 -0700

    fill in auto-scaling functionality, add config file

---
+++ asg.conf.sh
@@ -0,0 +1,8 @@
+declare ASG_NAME='web-prod-asg'
+declare LAUNCH_NAME='web-prod-lc'
+declare INSTANCE_NAME='xybersolve-prod-web2'
+declare INSTANCE_ID=''
+declare NAME='web-prod'
+declare MIN=1
+declare MAX=2
+declare DESIRED=1
